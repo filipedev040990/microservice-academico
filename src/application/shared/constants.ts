01db0604a94c1d1f3bc3821b86640a9c781f4324
Filipe Siqueira <filipedev040990@gmail.com>
2023-03-30 11:50:56 -0300

    feat: implements mail service with mailTrap

--- src/application/shared/constants.ts
+++ src/application/shared/constants.ts
@@ -7,6 +7,14 @@ export default {
     enrollment: {
       subject: 'Acesso liberado.',
       html: 'Seu pagamento foi confirmado e o acesso ao nosso liberado. Para acessar, acesse este <a href= "localhost:8080/acesso">link</a>. Seus login e senha são seu CPF'
+    },
+    auth: {
+      user: '7618da334575c0',
+      pass: '53e14bb87f9207'
+    },
+    server: {
+      host: 'smtp.mailtrap.io',
+      port: 2525
     }
   }
 }
---
+++ src/infra/adapters/mail/mailTrap.ts
@@ -0,0 +1,34 @@
+import { SendMail } from '@/application/contracts/mail'
+import Mail from 'nodemailer/lib/mailer'
+import nodemailer from 'nodemailer'
+import constants from '@/application/shared/constants'
+
+export class MailService implements SendMail {
+  private readonly transporter: Mail
+
+  constructor () {
+    this.transporter = nodemailer.createTransport({
+      host: process.env.MAILTRAP_HOST ?? constants.mailConfig.server.host,
+      port: +process.env.MAILTRAP_PORT ?? constants.mailConfig.server.port,
+      auth: {
+        user: process.env.MAILTRAP_USER ?? constants.mailConfig.auth.user,
+        pass: process.env.MAILTRAP_PASS ?? constants.mailConfig.auth.pass
+      }
+    })
+  }
+
+  async send (input: SendMail.Input): Promise<void> {
+    await this.transporter.sendMail({
+      to: {
+        name: input.to.name,
+        address: input.to.address
+      },
+      from: {
+        name: input.from.name,
+        address: input.from.address
+      },
+      subject: input.subject,
+      html: input.html
+    })
+  }
+}
