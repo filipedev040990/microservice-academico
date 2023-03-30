87ceb045c251041f629d137ca299c794cc279be7
Filipe Siqueira <filipedev040990@gmail.com>
2023-03-30 12:08:49 -0300

    feat: implements enrollment use case factory

---
+++ src/infra/factories/enrollent-usecase.ts
@@ -0,0 +1,15 @@
+import { EnrollmentUseCase } from '@/application/usecases/enrollment'
+import { MailService } from '@/infra/adapters/mail/mailTrap'
+import { AccessRepository } from '@/infra/database/repositories/access'
+import { AddressRepository } from '@/infra/database/repositories/address'
+import { EnrollmentsRepository } from '@/infra/database/repositories/enrollments'
+import { StudentRepository } from '@/infra/database/repositories/student'
+
+export const enrollmentUseCaseFactory = (): EnrollmentUseCase => {
+  const studentRepository = new StudentRepository()
+  const addressRepository = new AddressRepository()
+  const enrollmentRepository = new EnrollmentsRepository()
+  const accessRepository = new AccessRepository()
+  const mailService = new MailService()
+  return new EnrollmentUseCase(studentRepository, addressRepository, enrollmentRepository, accessRepository, mailService)
+}
